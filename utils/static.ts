1cc6e570ecd59bf57fcd41afc88dc694821402d3
Paul <molinpaul@live.fr>
2020-12-09 03:18:29 +0100

    UP

--- utils/static.ts
+++ utils/static.ts
@@ -2,17 +2,14 @@ import { serveFile } from 'https://deno.land/std@0.80.0/http/file_server.ts';
 
 const serveStatic = async (request: any, pub: string) => {
     const root = Deno.cwd();
-    const path = `${root}/${pub}${request.url}`;
+    let path = `${root}/${pub}${request.url}`;
     const folder = `${root}/${pub}`;
-    let file = `${folder}/index.html`;
-    try {
-        const read = await Deno.readFile(path);
-        const content = await serveFile(request, path);
-        request.respond(content);
-    } catch(error) {
-        const content = await serveFile(request, file);
-        request.respond(content);
+    if(!request.url.includes('.')) {
+        path = `${folder}/index.html`;
     }
+    const content = await serveFile(request, path);
+    request.respond(content);
+
 }
 
 export default serveStatic;
