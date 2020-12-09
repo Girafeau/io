79a5e289ddd916462a8a3501bc161a59d7b512ca
Paul <molinpaul@live.fr>
2020-12-09 05:27:51 +0100

    REFAC

--- server/utils/generator.ts
+++ server/utils/generator.ts
@@ -1,9 +1,9 @@
 const ta = await Deno.readTextFile('./server/files/animals.txt');
 const tc = await Deno.readTextFile('./server/files/colors.txt');
 const th = await Deno.readTextFile('./server/files/hexs.txt');
-const animals = ta.split('\r\n');
-const colors = tc.split('\r\n');
-const hexs = th.split('\r\n');
+const animals = ta.split('\n');
+const colors = tc.split('\n');
+const hexs = th.split('\n');
 
 const name = (): string => {
     const na = Math.floor(Math.random() * Math.floor(animals.length));
